25f58e5cb32ecd4309b45a6978b016aedf913b9d
Alfred Neumayer <dev.beidl@gmail.com>
2023-09-16 16:03:36 +0200

    src: Multithreaded SysrootManager with progress indication

--- src/utility/sysrootmanager.cpp
+++ src/utility/sysrootmanager.cpp
@@ -8,15 +8,91 @@
 #include <microtar.h>
 
 SysrootManager::SysrootManager(QObject *parent)
-    : QObject{parent}
+    : QObject{parent}, m_installing{false}, m_progress{0.0}
 {
+    QObject::connect(&m_installThread, &QThread::started, this, &SysrootManager::runInThread, Qt::DirectConnection);
+}
 
+SysrootManager::~SysrootManager()
+{
+    m_installThread.quit();
+    m_installThread.wait(1000);
+}
+
+void SysrootManager::setProgress(const qreal value)
+{
+    if (m_progress == value)
+        return;
+
+    m_progress = value;
+    emit progressChanged();
 }
 
 void SysrootManager::installBundledSysroot()
 {
+    m_installThread.start();
+}
+
+void SysrootManager::unpackTar(QString archive, QString target)
+{
+    mtar_t tar;
+    mtar_header_t tarHeader;
+    char *tarFileBuffer = nullptr;
+
+    static const unsigned TAR_TYPE_FILE = 48;
+
+    mtar_open(&tar, archive.toUtf8().data(), "r");
+    while ((mtar_read_header(&tar, &tarHeader)) != MTAR_ENULLRECORD) {
+        printf("%s (%d bytes, type %d)\n", tarHeader.name, tarHeader.size, tarHeader.type);
+
+        const auto temporaryPath = target + "/" + QString::fromUtf8(tarHeader.name, strlen(tarHeader.name));
+        const auto temporaryDirPath = QFileInfo(temporaryPath).absolutePath();
+        QDir temporaryDir(temporaryDirPath);
+
+        if (!temporaryDir.exists()) {
+            temporaryDir.mkpath(temporaryDirPath);
+        }
+
+        // Is the content a file?
+        if (tarHeader.type == TAR_TYPE_FILE) {
+            tarFileBuffer = new char[tarHeader.size];
+            mtar_read_data(&tar, tarFileBuffer, tarHeader.size);
+
+            QFile temporary(temporaryPath);
+            qDebug() << temporary.fileName();
+            if (!temporary.open(QFile::WriteOnly)) {
+                qWarning() << "Failed to open file for writing";
+                goto next;
+            }
+
+            if (temporary.write(tarFileBuffer, tarHeader.size) != tarHeader.size) {
+                qWarning() << "Failed to write size of" << tarHeader.size;
+                goto next;
+            }
+
+            qDebug() << "Unpacked" << temporaryPath;
+        }
+
+    next:
+        if (tarFileBuffer) {
+            delete[] tarFileBuffer;
+            tarFileBuffer = nullptr;
+        }
+        mtar_next(&tar);
+    }
+    mtar_close(&tar);
+}
+
+void SysrootManager::runInThread()
+{
     const QString temporaries = QStandardPaths::writableLocation(QStandardPaths::TempLocation) +
                                 QStringLiteral("/The-Sysroot");
+
+    m_installing = true;
+    emit installingChanged();
+
+    const int stages = 5;
+    int stage = 0;
 
     // Clear old temporaries
     {
@@ -24,69 +100,44 @@ void SysrootManager::installBundledSysroot()
         QDir targetDir(temporaries);
         if (targetDir.exists())
             qDebug() << targetDir.removeRecursively();
+        setProgress((qreal)stage++ / (qreal)stages);
     }
 
     // Unpack new temporaries
     {
         const auto archive = qApp->applicationDirPath() + "/the-sysroot.tar";
-        mtar_t tar;
-        mtar_header_t tarHeader;
-        char *tarFileBuffer = nullptr;
-
-        static const unsigned TAR_TYPE_FILE = 48;
-
-        mtar_open(&tar, archive.toUtf8().data(), "r");
-        while ((mtar_read_header(&tar, &tarHeader)) != MTAR_ENULLRECORD) {
-            printf("%s (%d bytes, type %d)\n", tarHeader.name, tarHeader.size, tarHeader.type);
-
-            const auto temporaryPath = temporaries + "/" + QString::fromUtf8(tarHeader.name, strlen(tarHeader.name));
-            const auto temporaryDirPath = QFileInfo(temporaryPath).absolutePath();
-            QDir temporaryDir(temporaryDirPath);
-
-            if (!temporaryDir.exists()) {
-                temporaryDir.mkpath(temporaryDirPath);
-            }
-
-            // Is the content a file?
-            if (tarHeader.type == TAR_TYPE_FILE) {
-                tarFileBuffer = new char[tarHeader.size];
-                mtar_read_data(&tar, tarFileBuffer, tarHeader.size);
-
-                QFile temporary(temporaryPath);
-                qDebug() << temporary.fileName();
-                if (!temporary.open(QFile::WriteOnly)) {
-                    qWarning() << "Failed to open file for writing";
-                    goto next;
-                }
-
-                if (temporary.write(tarFileBuffer, tarHeader.size) != tarHeader.size) {
-                    qWarning() << "Failed to write size of" << tarHeader.size;
-                    goto next;
-                }
-
-                qDebug() << "Unpacked" << temporaryPath;
-            }
-
-        next:
-            if (tarFileBuffer) {
-                delete[] tarFileBuffer;
-                tarFileBuffer = nullptr;
-            }
-            mtar_next(&tar);
-        }
-        mtar_close(&tar);
+        unpackTar(archive, temporaries);
+        setProgress((qreal)stage++ / (qreal)stages);
     }
+
+    // Boost
+    {
+        const auto archive = qApp->applicationDirPath() + "/boost.tar";
+        unpackTar(archive, temporaries + QStringLiteral("/Sysroot/include"));
+        setProgress((qreal)stage++ / (qreal)stages);
+    }
+
+    // SDL
+#if 0
+    {
+        const auto archive = qApp->applicationDirPath() + "/SDL.tar";
+        unpackTar(archive, temporaries + QStringLiteral("/Sysroot/include"));
+    }
+#endif
 
     // Clang parts
     {
         const QString source = temporaries + "/Clang";
-        const QString target = QStandardPaths::writableLocation(QStandardPaths::HomeLocation) +
-                               QStringLiteral("/Library/usr/lib/clang/14.0.0");
+        const QString targetRoot = QStandardPaths::writableLocation(QStandardPaths::HomeLocation) +
+                               QStringLiteral("/Library/usr/lib/clang");
+        const QString target = targetRoot + QStringLiteral("/17");
 
-        qDebug() << "Clearing old Clang area";
-        QDir targetDir(target);
-        if (targetDir.exists())
-            qDebug() << targetDir.removeRecursively();
+        {
+            qDebug() << "Clearing old Clang area";
+            QDir targetDir(targetRoot);
+            if (targetDir.exists())
+                qDebug() << targetDir.removeRecursively();
+        }
 
         qDebug() << "Moving bundled Clang headers";
         QDirIterator it(source, QDir::NoDotAndDotDot | QDir::AllEntries, QDirIterator::Subdirectories);
@@ -105,6 +156,7 @@ void SysrootManager::installBundledSysroot()
             qDebug() << "Moving" << relativePath << "from" << sourcePath << "to" << targetPath;
             QFile::rename(sourcePath, targetPath);
         }
+        setProgress((qreal)stage++ / (qreal)stages);
     }
 
     // Sysroot/wasi-sdk parts
@@ -135,5 +187,9 @@ void SysrootManager::installBundledSysroot()
             qDebug() << "Moving" << relativePath << "from" << sourcePath << "to" << targetPath;
             QFile::rename(sourcePath, targetPath);
         }
+        setProgress((qreal)stage++ / (qreal)stages);
     }
+
+    m_installing = false;
+    emit installingChanged();
 }
--- src/utility/sysrootmanager.h
+++ src/utility/sysrootmanager.h
@@ -2,18 +2,33 @@
 #define SYSROOTMANAGER_H
 
 #include <QObject>
+#include <QThread>
 
 class SysrootManager : public QObject
 {
     Q_OBJECT
+    Q_PROPERTY(bool installing MEMBER m_installing NOTIFY installingChanged FINAL)
+    Q_PROPERTY(qreal progress MEMBER m_progress NOTIFY progressChanged FINAL)
+
 public:
     explicit SysrootManager(QObject *parent = nullptr);
+    ~SysrootManager();
 
 public slots:
     void installBundledSysroot();
+    void runInThread();
+
+private:
+    void unpackTar(QString archive, QString target);
+    void setProgress(const qreal value);
+
+    bool m_installing;
+    qreal m_progress;
+    QThread m_installThread;
 
 signals:
-
+    void installingChanged();
+    void progressChanged();
 };
 
 #endif // SYSROOTMANAGER_H
